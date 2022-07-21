9983be1a41e38bdcf872a1ea56fd04d620f21be6
Saul Ventura <saulventura@Sauls-MacBook-Pro.local>
2022-07-20 22:13:38 -0500

    update resume

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,31 @@
 import React from "react";
+import projects from "./projects";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const Portfolio = () => {
-    return(<main className="container text-center border border-primary">
+
+    return (<main className="container text-center border border-primary">
         <h2 className="sectionheading">Portfolio</h2>
-    
+        {projects.map((ele, key) => {
+            return (<>
+
+                <Card key={key} style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={process.env.PUBLIC_URL + ele.screenshot} />
+                    <Card.Body>
+                        <Card.Title>{ele.title}</Card.Title>
+                        <Card.Text>
+                            {ele.description}
+                        </Card.Text>
+                        <Button variant="primary"><a href={ele.Github}>Github</a></Button>
+                        <Button variant="success"><a className = "text-white" href={ele.live}>live</a></Button>
+
+
+                    </Card.Body>
+                </Card>
+
+            </>)
+        })}
     </main>)
 }
 
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const Resume = () => {
-    return(<main className="container text-center border border-primary">
+    return(<main  className="container text-center border border-primary">
         <h2 className="sectionheading">Resume</h2>
     
+        <iframe id="resume" src="https://docs.google.com/document/d/e/2PACX-1vQDKFXhgMT8xxojCD04l8chgI4dLs3y2qgo57uomQ0K_7HYlSG4QPk20fr1lTZltPMWdnu79AYe8rOJ/pub?embedded=true"></iframe>
     </main>)
 }
 
